4a12b9144d89f9e8da372db66f423e70b2d2b27c
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2018-07-05 20:41:31 +0900

    User Serializer

---
+++ twit/twit/users/serializers.py
@@ -0,0 +1,60 @@
+from rest_framework import serializers
+from . import models
+from twit.tweets import models as tweet_models
+from twit.tweets import serializers as tweet_serializers
+
+
+class TwitSerializer(serializers.ModelSerializer):
+
+    class Meta :
+        model = tweet_models.Tweet
+        fields = (
+            'id',
+            'twits',
+            'create_at',
+            'update_at',
+            'file',
+            'content',
+            'creator',
+            'tweet_count',
+            'like_count',
+            'retweet_count'
+        )
+
+
+class SmallFollowListSerializer(serializers.ModelSerializer):
+
+    class Meta : 
+        model = models.User
+        fields = (
+            'profile_image',
+            'username',
+            'name',
+        )
+
+
+class AllFollowListSerializer(serializers.ModelSerializer):
+
+    class Meta :
+        model = models.User
+        fields = (
+            'profile_image',
+            'username',
+            'name',
+            'bio',
+        )
+
+class SmallProfileSerializer(serializers.ModelSerializer):
+
+    class Meta :
+        model = models.User
+        fields = (
+            'header_image',
+            'profile_image',
+            'username',
+            'name',
+            'bio',
+            'follower_count',
+            'following_count',
+            'tweet_count',
+        )
